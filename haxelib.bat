d338938e13404fe3e07fc4450a7ff8d606a652f9
Crisis <102416852+bambitheone82112@users.noreply.github.com>
2022-08-07 10:14:35 +0700

    ye

---
+++ haxelib.bat
@@ -0,0 +1,17 @@
+@echo off
+echo haxelib install lime 7.9.0
+echo haxelib install openfl
+echo haxelib install flixel
+echo haxelib run lime setup
+echo haxelib run lime setup flixel
+echo haxelib install flixel-tools
+echo haxelib run flixel-tools setup
+echo haxelib install flixel-addons
+echo haxelib install flixel-ui
+echo haxelib install hscript
+echo haxelib install newgrounds
+echo haxelib install linc_luajit
+echo haxelib git faxe https://github.com/uhrobots/faxe
+echo haxelib git polymod https://github.com/larsiusprime/polymod.git
+echo haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rp
+:: download git and haxe/haxelib first
